c3114984d514af5f7b23f4aa3a45938afb0dc0bb
Alvin Nava <anava8@mail.greenriver.edu>
2019-04-26 16:39:38 -0400

    Define a function that validates color, returns true if color is in array of valid colors, false otherwise

--- model/validation-functions.php
+++ model/validation-functions.php
@@ -1,1 +1,13 @@
 <?php
+
+/* Validate a color
+ *
+ * @param String color
+ * @return boolean
+ */
+
+function validColor($color)
+{
+    global $f3;
+    return in_array($color, $f3->get('colors'));
+}
